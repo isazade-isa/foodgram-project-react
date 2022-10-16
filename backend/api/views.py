30a70b582156bd71b6be260e44333f165c25338c
Исазаде Иса <isazade-isa@yandex.com>
2022-10-16 13:07:31 +0300

    final003

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from recipes.models import (Cart, Favorite, Ingredient, Recipe, Tag)
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import (SAFE_METHODS, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
+from recipes.models import (Cart, Favorite, Ingredient, Recipe, Tag)
 from users.models import Follow, User
 
 from .filters import IngredientSearchFilter, RecipeFilterSet
