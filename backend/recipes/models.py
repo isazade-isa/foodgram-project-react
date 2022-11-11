9a01076add04b7693cca8f6033c8b4bf377046a4
Исазаде Иса <isazade-isa@yandex.com>
2022-11-11 17:28:07 +0300

    final077

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -60,8 +60,8 @@ class Ingredient(models.Model):
             )
         ]
 
-    # def __str__(self):
-    #     return f'{self.name}, {self.measurement_unit}'
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
 
 
 class Recipe(models.Model):
