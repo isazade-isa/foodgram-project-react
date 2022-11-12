5da957c7bc7c68e6cd340f18afc3ca48dc658d4c
Исазаде Иса <isazade-isa@yandex.com>
2022-11-12 12:16:16 +0300

    final085

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -164,7 +164,7 @@ class CreateRecipeSerializer(ModelSerializer):
             ) for ingredient in ingredients
         ])
 
-    def validate(self, data):
+    def validate_ingredients(self, data):
         ingredients = self.initial_data.get('ingredients')
         ingredients_list = []
         for ingredient in ingredients:
@@ -174,8 +174,9 @@ class CreateRecipeSerializer(ModelSerializer):
                     'Есть задублированные ингредиенты!'
                 )
             ingredients_list.append(ingredient_id)
+        return data
 
-    def validate(self, data):
+    def validate_cooking_time(self, data):
         if int(data['cooking_time']) <= 0:
             raise ValidationError(
                 'Время приготовления должно быть больше 0!'
