aab99871d9d18ecd344a7586b47d3ed0981205e7
Исазаде Иса <isazade-isa@yandex.com>
2022-11-12 22:48:03 +0300

    final104

--- backend/api/views.py
+++ backend/api/views.py
@@ -105,7 +105,7 @@ class RecipeViewSet(ModelViewSet):
     @action(
         detail=False, methods=['get'], permission_classes=(IsAuthenticated,)
     )
-    def shopping_list():
+    def download_shopping_cart():
         return download_shopping_cart()
 
     @action(detail=True, methods=['post'])
